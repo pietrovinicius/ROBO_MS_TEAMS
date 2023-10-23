9436272e589731e69189c360796dfebe5f6d4b71
Pietro Lima <pietrovinicius@hotmail.com>
2023-10-23 16:58:24 -0300

    Correção na data e hora registrada
    Correção na data e hora registrada junto com o contador no TXT

--- ROBO - MS_TEAMS.py
+++ ROBO - MS_TEAMS.py
@@ -14,12 +14,16 @@ py.FAILSAFE = False
 varia = True
 chave = True
 contador = 0
-agora = datetime.datetime.now()
-agora = agora.strftime("%d/%m/%Y %H:%M:%S")
+
 def pausa(tempo):
     #print("time de:" , tempo)
     print("Aguarde... " , tempo , "s")
     time.sleep(tempo)
+    
+def agora():
+    agora = datetime.datetime.now()
+    agora = agora.strftime("%d/%m/%Y %H:%M:%S")
+    return str(agora)
 
 contador_ultimo = 0
 #abrindo log.txt para leitura
@@ -43,8 +47,8 @@ try:
                         
     with open("log.txt" , "a") as log:
         #log.write(f"\nAgora: {str(agora)}")           
-        print("Agora: " , str(agora))            
-        py.alert(title="==== Início ====" , text="Robo Inicializado: " + str(agora) , timeout=5000) 
+        print("Agora: " , str(agora()))            
+        py.alert(title="==== Início ====" , text="Robo Inicializado: " + str(agora()) , timeout=5000) 
         pausa(1)
         print("hotkey win")
         py.hotkey("win")
@@ -58,28 +62,28 @@ try:
         try:    
             while chave:
                 #tempo para clicar nas 2 opções:
-                pausa(5)
+                pausa(30)
                 if varia:
                     contador = contador + 1
-                    print(f"TEAMS ICONE PIETROW(346,254): {str(contador)}x")
-                    log.write(f"\n{agora}: {str(contador)}x")
+                    print(f"TEAMS PIETROW(346,254) - {agora()}:  {str(contador)}x")
+                    log.write(f"\n{agora()}: {str(contador)}x")
                     #py.click(346,254 , duration=1)            
                     py.click(148,314)
                     varia=False
                 elif (varia==False):
                     contador = contador + 1
-                    print(f"TEAMS ICONE RECENTE(232,443): {str(contador)}x")  
-                    log.write(f"\n{agora}: {str(contador)}x")                       
+                    print(f"TEAMS RECENTE(232,443) - {agora()}:  {str(contador)}x")  
+                    log.write(f"\n{agora()}: {str(contador)}x")                       
                     #py.click(232,443 , duration=1)
                     py.click(223,174 , duration=1) 
                     varia=True
         except KeyboardInterrupt:
             log.close()
-            print("log.close()\n==================================== FIM ====================================")
-            print("Interrompido pelo ctrl + c!!!")
+            print("Interrompido pelo ctrl + c!!!")            
+            print(f"log.close()\n{agora()}:  {str(contador)}x\n==================================== FIM ====================================")
         except Exception as erro:
             log.close()
-            print(f"log.close()\nErro: {erro=}, {type(erro)=}")                  
+            print(f"log.close()\n{agora()}\nErro: {erro=}, {type(erro)=}")                  
     log.close()
 except Exception as erro:
-   print(f"Error: {erro}")  
+   print(f"\n{agora()}\nError: {erro}")  
