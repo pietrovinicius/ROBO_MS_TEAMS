86930ac09d4b1c9839436bcf51fc5ea4c665a7a2
Pietro Lima <pietrovinicius@hotmail.com>
2023-10-23 15:37:17 -0300

    Persistindo dados com log.txt
    Dados de execução do app persistido com arquivo log.txt local, a contagem é salva e posteriormente usada em nova execução do app.

--- ROBO - MS_TEAMS.py
+++ ROBO - MS_TEAMS.py
@@ -5,6 +5,8 @@ import pyautogui as py
 import pandas
 import datetime
 import time
+import re
+import os
 import sys
 
 #para sair da automacao colocando o mouse no topo a esquerda da janela
@@ -12,36 +14,72 @@ py.FAILSAFE = False
 varia = True
 chave = True
 contador = 0
+agora = datetime.datetime.now()
+agora = agora.strftime("%d/%m/%Y %H:%M:%S")
 def pausa(tempo):
     #print("time de:" , tempo)
+    print("Aguarde... " , tempo , "s")
     time.sleep(tempo)
-        
-print("============================== inicio ========================")
-agora = datetime.datetime.now()    
-print("Agora: " , str(agora))    
-agora = agora.strftime("%d/%m/%Y %H:%M:%S")
-py.alert(title="==== Início ====" , text="Robo Inicializado: " + str(agora) , timeout=5000) 
-pausa(1)
-print("hotkey win")
-py.hotkey("win")
-pausa(1)
-print("Escrevendo teams")
-py.write("teams")
-pausa(1)
-print("Apertando enter;")
-py.hotkey("enter")
-while chave:
-    #tempo para clicar nas 2 opções:
-    time.sleep(15)
-    if varia:
-        contador = contador + 1
-        print(f"TEAMS ICONE PIETROW(346,254): {contador}x")
-        #py.click(346,254 , duration=1)            
-        py.click(148,314)
-        varia=False
-    elif (varia==False):
-        contador = contador + 1
-        print(f"TEAMS ICONE RECENTE(232,443): {contador}x")     
-        #py.click(232,443 , duration=1)
-        py.click(223,174 , duration=1) 
-        varia=True
+
+contador_ultimo = 0
+#abrindo log.txt para leitura
+with open("log.txt" , "r") as log:
+    print("Abrindo log.txt para leitura;")
+    for linha in log:
+        #print(linha)
+        contador_ultimo = linha
+    contador_ultimo = contador_ultimo.split()
+    contador_ultimo = contador_ultimo[-1]
+    contador_ultimo = re.sub(r"\D","",contador_ultimo)
+    contador = int(contador_ultimo)
+    print(f"Contador com o sub: {contador_ultimo}\nContador atual: {contador}")
+    print(type(contador))
+
+try:              
+    print("============================== inicio ========================")
+    if not os.path.exists("log.txt"):
+            with open("log.txt" , "a") as log:
+                log.write("")
+                        
+    with open("log.txt" , "a") as log:
+        #log.write(f"\nAgora: {str(agora)}")           
+        print("Agora: " , str(agora))            
+        py.alert(title="==== Início ====" , text="Robo Inicializado: " + str(agora) , timeout=5000) 
+        pausa(1)
+        print("hotkey win")
+        py.hotkey("win")
+        pausa(1)
+        print("Escrevendo teams")
+        py.write("teams")
+        pausa(1)
+        print("Apertando enter;")
+        py.hotkey("enter")
+
+        try:    
+            while chave:
+                #tempo para clicar nas 2 opções:
+                pausa(5)
+                if varia:
+                    contador = contador + 1
+                    print(f"TEAMS ICONE PIETROW(346,254): {str(contador)}x")
+                    log.write(f"\n{agora}: {str(contador)}x")
+                    #py.click(346,254 , duration=1)            
+                    py.click(148,314)
+                    varia=False
+                elif (varia==False):
+                    contador = contador + 1
+                    print(f"TEAMS ICONE RECENTE(232,443): {str(contador)}x")  
+                    log.write(f"\n{agora}: {str(contador)}x")                       
+                    #py.click(232,443 , duration=1)
+                    py.click(223,174 , duration=1) 
+                    varia=True
+        except KeyboardInterrupt:
+            log.close()
+            print("log.close()\n==================================== FIM ====================================")
+            print("Interrompido pelo ctrl + c!!!")
+        except Exception as erro:
+            log.close()
+            print(f"log.close()\nErro: {erro=}, {type(erro)=}")                  
+    log.close()
+except Exception as erro:
+   print(f"Error: {erro}")  
